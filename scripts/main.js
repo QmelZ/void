a4f823fee70a994253308be51ebc68d1e95efdc1
QmelZ <qmelzthevoid@gmail.com>
2020-12-22 01:54:15 +0300

    uninformative commit name

--- scripts/main.js
+++ scripts/main.js
@@ -7,24 +7,19 @@ if(!Vars.headless){
     });
 };
 
-var versionUrl = "https://gist.githubusercontent.com/QmelZ/6f96230874381bcdb57bca00651396ef/raw/41d57b98077b10bd46901e2ac70cf7a01ebd0be0/h";
+/*
+var versionUrl = "h";
 Core.net.httpGet(versionUrl, (response) => {
     var version = response.getResultAsString();
 }, (error) => {
     var version = "[scarlet]Error getting the version info[]";
 });
 Events.on(ClientLoadEvent, () => {
-//     Vars.ui.loadfrag.show();
+    Vars.ui.loadfrag.show();
     
 });
+*/
 
 // require stuff
 require("units/shockwave");
 require("blocks/voidron-collider");
-
-/*
-    Vars.ui.showText(
-        "Update",
-        "[#8f00ff]Void[] is still actively developing, so it is highly suggested that you update to the latest version.\n\nThe latest version is: " + version
-    );
-*/
