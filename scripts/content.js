14e9e9aca5fc4330332788dd96219cfbc34d3687
QmelZ <qmelzthevoid@gmail.com>
2021-05-09 18:03:31 +0300

    best content loader ever made

---
+++ scripts/content.js
@@ -0,0 +1,26 @@
+importPackage(Packages.arc.util.async);
+
+let ignored = [
+    "selenia", "voidpad", "aquashield"
+];
+
+function load(){
+    let scripts = [];
+    Threads.daemon(() => {
+        let dir = Vars.mods.getMod(modName).root.child("scripts");
+        dir.child("content").walk(e => {
+            let tmp = e.absolutePath().split(modName);
+            tmp[0] = "";
+            tmp = tmp.join(modName).replace("/scripts", "");
+            let path = tmp.substring(0, tmp.length - 3);
+            scripts.push(path);
+        });
+    }).join();
+    scripts.forEach(e => {
+        let tmp = e.split("/");
+        if(ignored.includes(tmp[tmp.length - 1])) return;
+        require(e);
+    });
+}
+
+load();
--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,15 @@
+require(modName + "/content");
+
+/*
 Events.on(ClientLoadEvent, () => {
-    if(!Vars.headless){
-        Core.app.post(() => {
-            Vars.mods.locateMod("void").meta.displayName = "[#8f00ff]Void[]"
+    let name = "Void", color = "#8f00ff";
+    Vars.ui.mods.shown(() => {
+        Vars.ui.mods.children.get(1).children.get(3).children.get(0).children.each(e => {
+            let text = e.children.get(0).children.get(1).children.get(0).text;
+            if(text.subSequence(8, name.length - 8) === name){
+                text.replace(1, 7, color);
+            };
         });
-    }
+    });
 });
+*/
